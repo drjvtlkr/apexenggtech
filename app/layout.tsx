8f2b8b04762d38e75f6790f2282946b3b29a2959
drjvtlkr <dheerajrvithalkar@gmail.com>
2025-01-22 13:06:49 +0530

    navbar footer cursor added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Montserrat, Roboto, Smooch_Sans } from "next/font/google";
+import { 
+  // Montserrat, Roboto,
+   Smooch_Sans } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Cursor from "@/components/Cursor";
 import Footer from "@/components/Footer";
 
-const roboto = Roboto({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-  display: "swap",
-});
+// const roboto = Roboto({
+//   weight: ["400", "700"],
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
 const smooch = Smooch_Sans({
   subsets : ["latin"],
 })
 
-const most = Montserrat({
-  subsets:['latin']
-})
+// const most = Montserrat({
+//   subsets:['latin']
+// })
 
 export const metadata: Metadata = {
   title: "Apex Engineering Technology",
