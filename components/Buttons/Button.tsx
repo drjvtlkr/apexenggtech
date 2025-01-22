7dd1dfe945fd54fd7c58cb69744722645f40489c
jagadish011 <munavallijagadish@gmail.com>
2025-01-22 18:04:22 +0530

    added the mission and vision content in 3d cards

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -11,7 +11,7 @@ const Button: React.FC<ButtonProps> = ({ label, link }) => {
   return (
     <Link href={link} passHref className='outline-none'>
       <div
-        className="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg text-black bg-white backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#f69b3f] hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group outline-none focus:outline-none"
+        className="flex justify-center gap-2 items-center mx-auto shadow-xl text-2xl text-black bg-white backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-[#f69b3f] hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group outline-none focus:outline-none"
       >
         {label}
         <FaArrowRight className="w-8 h-8 justify-end group-hover:-rotate-2 group-hover:bg-white text-black ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 -rotate-45" />
--- components/MissionVisionCard.tsx
+++ components/MissionVisionCard.tsx
@@ -1,40 +1,39 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
-import image3 from "../images/continuousVacuumPan.jpg";
+import Image, { StaticImageData } from "next/image";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 interface MissionVisionCardProps {
   title: string;
   description: string;
-  // link: string;
+  image: StaticImageData; 
 }
 
-const MissionVisionCard = ({ title, description }: MissionVisionCardProps) => {
+const MissionVisionCard = ({ title, description, image }: MissionVisionCardProps) => {
   return (
     <CardContainer>
-      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+      <CardBody className="bg-white shadow-lg relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-3xl font-bold text-neutral-600 dark:text-white"
         >
           {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className="text-black text-xl max-w-sm mt-2 dark:text-neutral-300"
         >
           {description}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            src={image3.src}
+            src={image}
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-            alt="Mission Vision"
+            alt={title}
           />
         </CardItem>
       </CardBody>
