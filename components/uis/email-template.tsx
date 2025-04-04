a530b789b32421c2a18ada15cdc45f6d7122e4a8
drjvtlkr <dheerajrvithalkar@gmail.com>
2025-04-05 00:18:23 +0530

    final

--- components/uis/email-template.tsx
+++ components/uis/email-template.tsx
@@ -10,13 +10,13 @@ export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   <div>
     <h1>Welcome, {fullName}!</h1>
     <h2>Thank you for getting in touch with us</h2>
-    <p>W make is a point to get back to you ASAP.</p>
+    <p>We make it a point to get back to you ASAP.</p>
     <br />
     <p>
       We are specialized in designing and manufacturing special machinery and
       related components to Sugar Industries. We pride ourselve for being in
-      this industry for more than 12 years. You took the best decision by
-      getting in touch with us for your Industrial needs
+      this industry for more than 12 years. You, undoubtedly have taken the best decision by
+      getting in touch with us for your Suagr Industrial needs. 
     </p>
   </div>
 );
